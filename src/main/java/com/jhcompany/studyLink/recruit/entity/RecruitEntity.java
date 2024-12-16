52962c8ef9ed08e94c1ce27437f58bc8f368b89f
DESKTOP-8GNVN33\Jeongkyohoon <jungkyohoon3841@gmail.com>
2024-12-17 00:03:40 +0900

    recruitEntity create

---
+++ src/main/java/com/jhcompany/studyLink/recruit/entity/RecruitEntity.java
@@ -0,0 +1,35 @@
+package com.jhcompany.studyLink.recruit.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "recruit_post")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecruitEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postIndex;
+
+    @Column(columnDefinition = "varchar(50) comment '유저ID'", nullable = false)
+    private String cid;
+
+    @Column(columnDefinition = "varchar(255) comment '제목'", nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "varchar(500) comment '내용'")
+    private String content;
+
+    @Column(name = "poster_tag", columnDefinition = "varchar(500) comment '기술태그'")
+    private String posterTag;
+
+    @Column(columnDefinition = "varchar(10) comment '마감기한'", nullable = false)
+    private String deadLine;
+
+    @Column(name = "reqruiter_number", columnDefinition = "int(11) comment '모집인원'", nullable = false)
+    private int reqruiterNumber;
+}
